5914b9e95946b1af7480ac5d0a8da6cd8b333b10
atlednolispe <atlednolispe@gmail.com>
2017-10-19 14:43:15 +0800

    1.1.1.Give the value

---
+++ src/bookexercises/chapter1/fundamentals/One.java
@@ -0,0 +1,17 @@
+package bookexercises.chapter1.fundamentals;
+
+/**
+ * 1.1.1
+ *
+ * Give the value of each of the following expression:
+ * a. ( 0 + 15 ) / 2
+ * b. 2.0e-6 * 100000000.1
+ * c. true && false || true && true
+ */
+public class One {
+    public static void main(String[] args) {
+        System.out.println(( 0 + 15 ) / 2);
+        System.out.println(2.0e-6 * 100000000.1);
+        System.out.println(true && false || true && true);
+    }
+}
