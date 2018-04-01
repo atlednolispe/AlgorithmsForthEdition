ecfaa0e24d294c4389edfb2d11802ca2776d46c7
atlednolispe <atlednolispe@gmail.com>
2018-04-01 16:03:18 +0800

    1.1.34.Filtering

---
+++ src/bookexercises/chapter1/fundamentals/ThirtyFour.java
@@ -0,0 +1,26 @@
+package bookexercises.chapter1.fundamentals;
+
+/**
+ * Filtering. Which of the following require saving all the values from standard
+ * input (in an array, say), and which could be implemented as a filter using only a fixed
+ * number of variables and arrays of fixed size (not dependent on N)? For each, the input
+ * comes from standard input and consists of N real numbers between 0 and 1.
+ *
+ * 0: require saving all the values
+ * 1: could be implemented as a filter
+ *
+ * 1 Print the maximum and minimum numbers.
+ * 0 Print the median of the numbers.
+ * 1 Print the k th smallest value, for k less than 100.
+ * 1 Print the sum of the squares of the numbers.
+ * 1 Print the average of the N numbers.
+ * 0 Print the percentage of numbers greater than the average.
+ * 0 Print the N numbers in increasing order.
+ * 0 Print the N numbers in random order.
+ *
+ * @author atlednolispe
+ * @email atlednolispe@gmail.com
+ * @date 2018/4/1
+ */
+public class ThirtyFour {
+}
