ea3d0c65d30737cada694f419344403e370ee5f0
atlednolispe <atlednolispe@gmail.com>
2018-06-30 22:54:15 +0800

    1.1.5.String method don't modify original string

---
+++ src/bookexercises/chapter1/fundamentals/two/Five.java
@@ -0,0 +1,19 @@
+package bookexercises.chapter1.fundamentals.two;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * What does the following code fragment print?
+ *
+ * @author atlednolispe
+ * @email atlednolispe@gmail.com
+ * @date 2018/6/30
+ */
+public class Five {
+    public static void main(String[] args) {
+        String s = "Hello World";
+        s.toUpperCase();
+        s.substring(6, 11);
+        StdOut.println(s);
+    }
+}
